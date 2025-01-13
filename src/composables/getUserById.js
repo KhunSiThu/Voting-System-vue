755ae059d6c802233ffe254625fd1e2a178fd26a
KhunSiThu <khunsithuaung65@gmail.com>
2025-01-13 10:16:33 +0630

    jan131016

--- src/composables/getUserById.js
+++ src/composables/getUserById.js
@@ -10,7 +10,7 @@ const getUserById = (id) => {
 
     const load = async () => {
         try {
-            const response = await fetch(" http://localhost:3000/users/" + id);
+            const response = await fetch(" http://0.0.0.0:3000/users/" + id);
             if (response.status === 404) {
                 throw new Error("URL Not Found!");
             }
--- src/views/ProfileForm.vue
+++ src/views/ProfileForm.vue
@@ -154,7 +154,7 @@ export default {
                 };
 
                 try {
-                    const response = await fetch(" http://localhost:3000/users", {
+                    const response = await fetch(" http://0.0.0.0:3000/users", {
                         method: "POST",
                         headers: {
                             "Content-Type": "application/json"
@@ -185,7 +185,7 @@ export default {
                 }
 
                 try {
-                    const response = await fetch('http://localhost:9000/public/controllers/sendVerifyCode.php', {
+                    const response = await fetch('http://0.0.0.0:9000/public/controllers/sendVerifyCode.php', {
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/json'
